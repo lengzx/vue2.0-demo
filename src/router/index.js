f18a943a320c060ce6dafd0ca810c2c754b14bb9
lengzx <981143260@qq.com>
2018-04-16 09:33:35 +0800

    vue2.0-demo

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '../App'
+import goods from '../components/goods/goods'
+import ratings from '../components/ratings/ratings'
+import seller from '../components/seller/seller'
+
+import '../common/stylus/index.styl'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: goods,
+    },
+    {
+      path: '/goods',
+      component: goods
+    },
+    {
+      path: '/ratings',
+      component: ratings
+    }, {
+      path: '/seller',
+      component: seller
+    }
+  ]
+})
